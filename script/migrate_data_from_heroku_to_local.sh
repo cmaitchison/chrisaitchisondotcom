657234881d76b240c8424397f08fc712757e097a
cmaitchison <cmaitchison@gmail.com>
2011-05-11 22:46:55 +1000

    Added some explanatory comments to db_migration script

--- script/migrate_data_from_heroku_to_local.sh
+++ script/migrate_data_from_heroku_to_local.sh
@@ -1,3 +1,6 @@
+#!/bin/bash -ex
+# This script asks Heroku to backup the current production data, then downloads that dump and imports it into the local db.
+# It is at least 3 times quicker than heroku db:pull, and we can only do it because we are dumping from postgres to postgres.
 heroku pgbackups:capture --expire
 curl -o tmp/latest.dump `heroku pgbackups:url`
 pg_restore --verbose --clean --no-acl --no-owner -d chrisaitchisondotcom_dev tmp/latest.dump 
