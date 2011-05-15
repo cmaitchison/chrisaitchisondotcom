7fa91a63e46f7b10219086e5ad166f6a378a3e81
cmaitchison <cmaitchison@gmail.com>
2011-05-15 21:37:34 +1000

    Changed staging environment logging level to debug

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,6 +6,7 @@ Enki::Application.configure do
   config.serve_static_assets = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
+  config.log_level = :debug
 end
 #Enki::Config.default[:disqus_name]="chrisaitchisondotcom-staging"
 #Enki::Config.default[:disqus_url]="chrisaitchisondotcom-staging.heroku.com"
