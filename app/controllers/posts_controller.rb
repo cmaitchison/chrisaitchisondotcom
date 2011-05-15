e37ea34e372539cf2fafa07ce914a0b15b9672fe
cmaitchison <cmaitchison@gmail.com>
2011-05-15 18:55:35 +1000

    Added HTTP cache-control headers to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    response.headers['Cache-Control'] = 'public, max-age=300'
     @tag = params[:tag]
     @posts = Post.find_recent(:tag => @tag, :include => :tags)
 
@@ -10,6 +11,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    response.headers['Cache-Control'] = 'public, max-age=300'
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {:include => [:approved_comments, :tags]}))
     @comment = Comment.new
   end
