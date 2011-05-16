b61dc84334f764d27a1e2559449f59608452b94a
cmaitchison <cmaitchison@gmail.com>
2011-05-16 22:20:43 +1000

    Inlined facebook like code into application.haml

--- app/helpers/facebook_helper.rb
+++
@@ -1,5 +0,0 @@
-module FacebookHelper
-  def facebook_like_button  
-    tag "iframe", {:src => "http://www.facebook.com/plugins/like.php?href=http://chrisaitchison.com",:scrolling => "no", :frameborder => "0",:style=>"border:none; width:250px; height:400px"}
-  end
-end    
