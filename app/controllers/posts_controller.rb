bf676f5783259a53446f98ce14b3c728ece264bc
cmaitchison <cmaitchison@gmail.com>
2011-05-16 22:51:10 +1000

    Changed the http cache timeout of the posts controller to 10 minutes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    response.headers['Cache-Control'] = 'public, max-age=86400'
+    response.headers['Cache-Control'] = 'public, max-age=600'
     @tag = params[:tag]
     @posts = Post.find_recent(:tag => @tag, :include => :tags)
 
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    response.headers['Cache-Control'] = 'public, max-age=86400'
+    response.headers['Cache-Control'] = 'public, max-age=600'
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {:include => [:approved_comments, :tags]}))
     @comment = Comment.new
   end
